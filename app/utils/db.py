e437f605de52ca27fef8d31d7128b4395567d725
marisiman <email_dari_marisiman@example.com>
2024-07-07 10:34:17 +0700

    added requirements files for integration into vercel x7

--- app/utils/db.py
+++ app/utils/db.py
@@ -20,8 +20,17 @@ DB_PORT = os.getenv("DB_PORT")
 
 # URI koneksi PostgreSQL
 # Pastikan DATABASE_URI diatur dengan benar sesuai dengan konfigurasi Anda
-DATABASE_URI = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-print(f"DATABASE_URI: {DATABASE_URI}")  # Debug print untuk memeriksa nilai DATABASE_URI
+# DATABASE_URI = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# print(f"DATABASE_URI: {DATABASE_URI}")  # Debug print untuk memeriksa nilai DATABASE_URI
+
+# Construct the DATABASE_URI if not explicitly set
+if not os.getenv('DATABASE_URI'):
+    DATABASE_URI = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+else:
+    DATABASE_URI = os.getenv('DATABASE_URI')
+
+# Debugging output
+print("Constructed DATABASE_URI:", DATABASE_URI)
 
 engine = create_engine(DATABASE_URI)
 Session = scoped_session(sessionmaker(bind=engine))
