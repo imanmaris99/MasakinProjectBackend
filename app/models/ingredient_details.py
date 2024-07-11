4fa0dcd1128cb65a36f6e820ac59cd5c4ed0f8dc
marisiman <email_dari_marisiman@example.com>
2024-07-11 05:59:40 +0300

    update tables relation and migration in database

---
+++ app/models/ingredient_details.py
@@ -0,0 +1,30 @@
+from sqlalchemy import DateTime, func
+from app.models.ingredient_name import IngredientName
+from app.utils.db import db
+
+class IngredientDetails(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    ingredient_name_id = db.Column(db.Integer, db.ForeignKey('ingredient_name.id'), nullable=False)
+    dose = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now()) 
+    updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())  
+
+    recipe = db.relationship("Recipes", backref='ingredientdetails')
+    ingredientname = db.relationship("IngredientName")
+
+    def as_dict(self):
+        # Dapatkan semua info bahan berdasarkan ingredient_name_id
+        ingredient_name_info = IngredientName.query.filter_by(id=self.ingredient_name_id).all()
+        ingredient_name_info_list = [ingredient_name.as_dict() for ingredient_name in ingredient_name_info] 
+
+        return {
+            'id': self.id,
+            'recipe_id': self.recipe_id,
+            'ingredient_name_info': ingredient_name_info_list if ingredient_name_info_list else None,
+            'ingredient_name_id': self.ingredient_name_id,
+            'dose': self.dose,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None
+        }
