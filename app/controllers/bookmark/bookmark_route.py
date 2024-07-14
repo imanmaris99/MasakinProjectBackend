f1de7a67d7d261779bcd02f205805e3f583ee43f
Blackpossum <yosaphatharwindra@gmail.com>
2024-07-14 15:18:54 +0700

    Feat: OP-get_bookmarks_by_ID, DEL_bookmarks_by_recipe_ID

--- app/controllers/bookmark/bookmark_route.py
+++ app/controllers/bookmark/bookmark_route.py
@@ -16,7 +16,20 @@ def get_bookmarks():
         return jsonify(list_my_recipe_data), 200
     except Exception as e:
         return jsonify({"message": str(e)}), 500
-    
+
+# Endpoint untuk mendapatkan bookmark berdasarkan user_id (GET)
+@bookmark_blueprint.route("/user/<int:user_id>", methods=["GET"])
+def get_bookmarks_by_user_id(user_id):
+    try:
+        bookmarks = Bookmarks.query.filter_by(user_id=user_id).all()
+        if bookmarks:
+            bookmark_data = [bookmark.as_dict() for bookmark in bookmarks]
+            return jsonify(bookmark_data), 200
+        else:
+            return jsonify({"message": "No bookmarks found for this user"}), 404
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
 @bookmark_blueprint.route("/new_bookmark", methods=["POST"])
 @jwt_required()
 def create_bookmark():
@@ -48,5 +61,22 @@ def create_bookmark():
 
     except KeyError as e:
         return jsonify({"message": f"Missing key: {str(e)}"}), 400
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+@bookmark_blueprint.route("/delete/<int:recipe_id>", methods=["DELETE"])
+@jwt_required()
+def delete_bookmark(recipe_id):
+    try:
+        current_user_id = get_jwt_identity()
+        bookmark = Bookmarks.query.filter_by(recipe_id=recipe_id, user_id=current_user_id).first()
+        
+        if bookmark:
+            db.session.delete(bookmark)
+            db.session.commit()
+            return jsonify({"message": "Bookmark deleted successfully"}), 200
+        else:
+            return jsonify({"message": "Bookmark not found"}), 404
+
     except Exception as e:
         return jsonify({"message": str(e)}), 500
